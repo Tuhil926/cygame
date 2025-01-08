721d50eedfe23b039d937fb314c80b2177bde42b
Tuhil <Tuhil>
2025-01-08 11:55:08 +0530

    added a plane to the demo scene

--- Object.cpp
+++ Object.cpp
@@ -3,10 +3,12 @@
 Object::Object(ShapeOnGPU shape_on_gpu) {
     this->shape_on_gpu = shape_on_gpu;
     position = glm::vec3(0.0f, 0.0f, -3.0f);
+    scale = glm::vec3(1.0f, 1.0f, 1.0f);
 }
 glm::mat4 Object::get_model_to_world_transform_matrix() {
     glm::mat4 translation_matrix = glm::translate(glm::mat4(1.0f), position);
-    return translation_matrix;
+    glm::mat4 scale_matrix = glm::scale(glm::mat4(1.0f), scale);
+    return translation_matrix * scale_matrix;
 }
 void Object::draw(Camera &camera) {
     camera.draw_shape(get_model_to_world_transform_matrix(),
--- demo_opengl.cpp
+++ demo_opengl.cpp
@@ -18,37 +18,12 @@ int main() {
     Shape *cube = ShapeGenerator::get_cube();
     Shape *sphere = ShapeGenerator::get_sphere(200, 100);
     // Shape *sphere = ShapeGenerator::get_cube();
-    Shape *monkey = ShapeGenerator::get_from_file("models/monkey.obj");
-    // for (int i = 0; i < monkey->vertex_count; i++) {
-    //     cout << "vert: " << monkey->vertices[NUM_FLOATS_PER_VERTEX * i +
-    //     0]
-    //          << ' ' << monkey->vertices[NUM_FLOATS_PER_VERTEX * i + 1] <<
-    //          ' '
-    //          << monkey->vertices[NUM_FLOATS_PER_VERTEX * i + 2] << endl;
-    //     cout << "colr: " << monkey->vertices[NUM_FLOATS_PER_VERTEX * i +
-    //     3]
-    //          << ' ' << monkey->vertices[NUM_FLOATS_PER_VERTEX * i + 4] <<
-    //          ' '
-    //          << monkey->vertices[NUM_FLOATS_PER_VERTEX * i + 5] << endl;
-    //     cout << "norm: " << monkey->vertices[NUM_FLOATS_PER_VERTEX * i +
-    //     6]
-    //          << ' ' << monkey->vertices[NUM_FLOATS_PER_VERTEX * i + 7] <<
-    //          ' '
-    //          << monkey->vertices[NUM_FLOATS_PER_VERTEX * i + 8] << endl;
-    //     cout << endl;
-    // }
-    // for (int i = 0; i < monkey->index_count; i++) {
-    //     cout << monkey->indices[i] << ' ';
-    //     if (i % 3 == 2)
-    //         cout << endl;
-    // }
-    // monkey->vertex_count = 4;
-    // monkey->index_count = 6;
+    Shape *plane = ShapeGenerator::get_from_file("models/plane.obj");
 
     ShapeRenderer renderer;
     auto cube_gpu = renderer.add_shape(cube);
     auto sphere_gpu = renderer.add_shape(sphere);
-    auto monkey_gpu = renderer.add_shape(monkey);
+    auto plane_gpu = renderer.add_shape(plane);
 
     // creating the shader program
 
@@ -60,11 +35,14 @@ int main() {
     Object cube_object_2(cube_gpu);
     Object cube_object_3(cube_gpu);
     Object sphere_object(sphere_gpu);
-    Object monkey_object(monkey_gpu);
+    Object plane_object(plane_gpu);
     cube_object_2.position.z = -7;
     cube_object_3.position.x = -7;
     sphere_object.position.z = -7;
-    monkey_object.position.x = 7;
+    plane_object.position.x = 7;
+    plane_object.scale.x = 7;
+    plane_object.scale.y = 7;
+    plane_object.scale.z = 7;
 
     while (running) {
         // you need to use this handle_event macro if you want to be able to use
@@ -109,7 +87,7 @@ int main() {
 
         sphere_object.draw(camera);
         cube_object.draw(camera);
-        monkey_object.draw(camera);
+        plane_object.draw(camera);
         // cube_object_2.draw(camera);
         // cube_object_3.draw(camera);
 
